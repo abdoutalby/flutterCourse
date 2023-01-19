dce530ea4d91bd8c6b14c8dbf0edc791dc68ba98
abdoutalby <abderrahmentalby@gmail.com>
2023-01-19 21:21:08 +0100

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Login()));
+  runApp(MaterialApp(
+      routes: {
+        "mainn": (context) => const Login(),
+        "mouty": (context) => const mouty(),
+        "oops": (context) => const OOPS(),
+      },
+      initialRoute: "mainn",
+      debugShowCheckedModeBanner: false,
+      home: const Login()));
+}
+
+class mouty extends StatefulWidget {
+  const mouty({super.key});
+
+  @override
+  State<mouty> createState() => _moutyState();
+}
+
+class _moutyState extends State<mouty> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          color: Colors.green,
+          child: ElevatedButton(
+            child: const Text("back"),
+            onPressed: () {
+              Navigator.pushNamed(context, "mainn");
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OOPS extends StatefulWidget {
+  const OOPS({super.key});
+
+  @override
+  State<OOPS> createState() => _OOPSState();
+}
+
+class _OOPSState extends State<OOPS> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.red,
+        child: Center(
+          child: ElevatedButton(
+            child: const Text("back"),
+            onPressed: () {
+              Navigator.pushNamed(context, "mainn");
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class Login extends StatefulWidget {
@@ -12,22 +74,39 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  TextEditingController userController = TextEditingController();
-  TextEditingController pwdController = TextEditingController();
-
+  late TextEditingController userController;
+  late TextEditingController pwdController;
   login() {
-    print("username ${userController.text} password ${pwdController.text} ");
+    String user = userController.text;
+    String pwd = pwdController.text;
+    if (user == "abdou" && pwd == "abdou") {
+      userController.clear();
+      pwdController.clear();
+      Navigator.pushNamed(context, "mouty");
+    } else {
+      userController.text = "error";
+      pwdController.text = "error";
+      Navigator.pushNamed(context, "oops");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    userController = TextEditingController();
+    pwdController = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
-            color: Colors.red,
-            height: 200,
+            color: Colors.black12,
+            height: 250,
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -51,7 +130,7 @@ class _LoginState extends State<Login> {
                       onPressed: () {
                         login();
                       },
-                      child: const Text("LOGIN"))
+                      child: const Text("LOGIN")),
                 ]),
           ),
         ),
